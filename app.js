368b63e730ec6b37a41fb4cd90b2f696ec74bfca
alik95997 <88495045+alik95997@users.noreply.github.com>
2024-12-21 20:59:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-// var input = document.getElementById("input");
-// var UI = document.getElementById("notesParent");
-// var arr = [];
-// var textOutput = document.getElementById("textOutput");
-
-// function renderUI() {
-//     arr.unshift(input.value);
-//     localStorage.setItem("key", JSON.stringify(arr));
-//     var a = JSON.parse(localStorage.getItem("key"));
-
-//     var notes = `<div class="notes">
-//                 <div class="notesText">
-//                     <p id="textOutput">${a[0]}</p>
-//                 </div>
-//                 <div class=".icons-btn">
-//                     <button onclick="editNotesText(this)"><i class="fa-regular fa-pen-to-square"></i></button>
-//                     <button onclick="deleteNotes(this)"><i class="fa-solid fa-trash"></i></button>
-//                 </div>
-//             </div>`
-//     UI.innerHTML += notes;
-
-//     console.log(a)
-// }
-// function editNotesText(editButton) {
-//     var editTextPrompt = prompt("Enter notes for replacing notes");
-//     editButton.parentNode.parentNode.firstElementChild.firstElementChild.innerHTML = editTextPrompt;
-// }
-// function deleteNotes(deleteButton) {
-//     deleteButton.parentNode.parentNode.remove();
-// }
 
 var input = document.getElementById("input");
 var notesParent = document.getElementById("notesParent");
